a77644c3e10b1dd8edad15aefe5f6e29ad35d062
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-04-25 17:20:40 +0900

    キャラ変更、歩きモーション、素材追加

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,31 +4,38 @@ using UnityEngine;
 public class Player_Con : MonoBehaviour
 {
     public float add_position;
+    private Animator animator;
     
     void Start()
     {
+        animator = GetComponent<Animator>();
+       
 
     }
 
     void Update()
     { 
-   
+        
         if(Input.GetKey(KeyCode.W))
         {
             transform.position = new Vector3(transform.position.x + add_position, transform.position.y + add_position, transform.position.z);
             this.GetComponent<SpriteRenderer>().flipX = false;
+            animator.SetBool("walk", true);
+            
         }
 
         if(Input.GetKey(KeyCode.S))
         {
             transform.position = new Vector3(transform.position.x - add_position, transform.position.y - add_position, transform.position.z);
             this.GetComponent<SpriteRenderer>().flipX = true;
+            animator.SetBool("walk", true);
         }
 
         if(Input.GetKey(KeyCode.D))
         {
             transform.position = new Vector3(transform.position.x + add_position, transform.position.y - add_position, transform.position.z);
             this.GetComponent<SpriteRenderer>().flipX = false;
+            animator.SetBool("walk", true);
         }
 
         if(Input.GetKey(KeyCode.A))
@@ -36,6 +43,12 @@ public class Player_Con : MonoBehaviour
             transform.position = new Vector3(transform.position.x - add_position, transform.position.y + add_position, transform.position.z);
            
             this.GetComponent<SpriteRenderer>().flipX = true;
+            animator.SetBool("walk", true);
+        }
+
+        if(!Input.anyKey)
+        {
+            animator.SetBool("walk", false);
         }
 
     }
